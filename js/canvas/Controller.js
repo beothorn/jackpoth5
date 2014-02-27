b2d8ad692b3eda199fca434576b5ef4a12b8996b
Lucas <lucass@gmail.com>
2014-02-27 14:02:50 -0300

    Removing seed

--- js/canvas/Controller.js
+++ js/canvas/Controller.js
@@ -1,9 +1,5 @@
 var runnersElement = document.getElementById("runners");
-var seedElement = document.getElementById("seed");
 var configElement = document.getElementById("config");
-var seededWidthElement = document.getElementById("seededWidth");
-var seededHeightElement = document.getElementById("seededHeight");
-var isSeededElement = document.getElementById("isSeeded");
 var generationIntervalElement = document.getElementById("generationInterval");
 var ballSpeedElement = document.getElementById("ballsSpeed");
 var prizesElement = document.getElementById("prizes");
@@ -14,8 +10,6 @@ var countdownElement = document.getElementById("countdown");
 playElement.style.display="none";
 
 var gameOptions = {};
-
-
 
 function getSearchParameters() {
       var prmstr = window.location.search.substr(1);
@@ -32,7 +26,23 @@ function transformToAssocArray( prmstr ) {
     return params;
 }
 
+function urlHasSeed(){
+	console.log(document.URL);
+	return false;
+}
 
+
+function getSeedFromUrl(){
+	return "foo";
+}
+
+function getWidthFromUrl(){
+	return 800;
+}
+
+function getHeightFromUrl(){
+	return 600;
+}
 
 playElement.onclick = function(){
  
@@ -47,19 +57,17 @@ playElement.onclick = function(){
 	
 	var dateNow = Date.now()+"";
 	var seed = dateNow.substr(dateNow.length-3,dateNow.length);
-	if(isSeededElement.checked){
-	    seed = seedElement.value;
-	       
-	    context.canvas.width  = seededWidthElement.value;
-		context.canvas.height = seededHeightElement.value;
+	
+	if(urlHasSeed()){
+	    seed = getSeedFromUrl();
+	    context.canvas.width  = getWidthFromUrl();
+		context.canvas.height = getHeightFromUrl();
 	}else{
 	    context.canvas.width  = window.innerWidth;
 		context.canvas.height = window.innerHeight;	
 	}
 		
 	gameOptions.seed = seed;
-	
-	gameOptions.hasSeed = isSeededElement.checked;
 	
 	gameOptions.generationInterval = generationIntervalElement.value;
 	gameOptions.ballSpeed = ballSpeedElement.value;
@@ -80,14 +88,6 @@ playElement.onclick = function(){
 	}); 
 	
     start();
-}
-
-isSeededElement.onclick = function(){
-    if(document.getElementById("isSeeded").checked){
-        document.getElementById("seedOptions").style.display="block";
-    }else{
-        document.getElementById("seedOptions").style.display="none";
-    }
 }
 
 function gameStopped(){
